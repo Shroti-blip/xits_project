e3105b4601694564bfd6885ec16e808bd5579e76
Muskan Shroti <shrotimuskan@gmail.com>
2025-12-08 22:54:18 +0530

    Enhance QuizAnswerRepository with completion queries
    Added methods to count completed quizzes for both users and check if a quiz has been completed.

--- ProjectHON/src/main/java/com/example/ProjectHON/Quiz_Answer/QuizAnswerRepository.java
+++ ProjectHON/src/main/java/com/example/ProjectHON/Quiz_Answer/QuizAnswerRepository.java
@@ -19,10 +19,27 @@ public interface QuizAnswerRepository extends JpaRepository<QuizAnswer, Integer>
     Optional<QuizAnswer>findPlayWithUserQuestionByUser(@Param("userBy")UserMaster userBy,
                                                        @Param("userTo")UserMaster userTo,
                                                        @Param("quizQuestion")QuizQuestion quizQuestion);
+//    Boolean existsByUserIdAndPlayedWithAndQuizCompleteTrue();
 
     @Query("SELECT COUNT(a) FROM QuizAnswer a WHERE a.user.userId = :userId AND a.playWith.userId = :playWithId")
     int countUserAttempts(Long userId, Long playWithId);
 
-    @Query(value = "select q from QuizAnswer q where (q.user.userId = :u1 And q.user.playWithId =: u2) OR (q.user.userId = :u2 And q.user.userId=:u1)")
+    @Query(value = "select q from QuizAnswer q where (q.user.userId = :u1 And q.playWith.userId =: u2) OR (q.user.userId = :u2 And q.playWith.userId=:u1)")
     List<QuizAnswer> getAllAnswersOfBoth(Long u1 , Long u2);
+
+    @Query("select count(q) from QuizAnswer q where q.user.userId=:u1 and q.playWith.userId=:u2 and q.quizComplete = true")
+    Long completeByUser1(Long u1, Long u2);
+
+    @Query("select count(q) from QuizAnswer q where q.user.userId=:u2 and q.playWith.userId=:u1 and q.quizComplete = true")
+    Long completedByUser2(Long u1, Long u2);
+
+
+
+//    Boolean existByUserIdAndPlayWithAndQuizCompleteTrue();
+
+
+    @Query("select count(a) >0 from QuizAnswer a where (a.user.userId =:u1 and a.playWith.userId =:u2 and a.quizComplete =true)")
+    boolean hasCompleted(Long u1 , Long u2);
+
+
 }
