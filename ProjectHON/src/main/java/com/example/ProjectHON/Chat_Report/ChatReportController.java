842f2f843bcd35a79e8d281caa4223d38345b4e3
Muskan Shroti <shrotimuskan@gmail.com>
2025-12-11 23:12:27 +0530

    Enhance reportChat with email notification
    Added email notification feature after reporting a chat.

--- ProjectHON/src/main/java/com/example/ProjectHON/Chat_Report/ChatReportController.java
+++ ProjectHON/src/main/java/com/example/ProjectHON/Chat_Report/ChatReportController.java
@@ -1,6 +1,8 @@
 package com.example.ProjectHON.Chat_Report;
 
 
+import com.example.ProjectHON.User_masterpackage.UserMaster;
+import com.example.ProjectHON.User_masterpackage.UserMasterRepository;
 import com.example.ProjectHON.Whisper_masterpackage.WhisperMaster;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
@@ -19,6 +21,12 @@ public class ChatReportController {
     @Autowired
     ChatReportService chatReportService;
 
+    @Autowired
+    ChatReportEmailService chatReportEmailService;
+
+    @Autowired
+    UserMasterRepository userMasterRepository;
+
 
     @PostMapping("/user/reportChat")
     public String saveData(@RequestParam("reason") String reason ,
@@ -29,12 +37,19 @@ public class ChatReportController {
                            RedirectAttributes redirectAttributes,
                            Model model){
        System.out.println("------inside reportChat mapping-------");
+       UserMaster owner = userMasterRepository.findById(ownerId).orElse(null);
        chatReportService.saveReportInfo(reason , comment ,ownerId,reporterId, whisperId);
-//       model.addAttribute("success" ,"Successfully Reported.");
+//       model.addAttribute("success" ,"Successfully Reported.");//use session.
         redirectAttributes.addFlashAttribute("success", "Successfully Reported.");
+        try{
+            chatReportEmailService.sendAfterGettingReportChat(owner.getEmail());
+        }catch (Exception e){
+            System.out.println("------Exception is in Email Sending--------");
+        }
 
         return "redirect:/user/whisper";
     }
+
 
 
 }
