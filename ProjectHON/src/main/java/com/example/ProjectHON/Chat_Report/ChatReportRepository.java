e93526650073dd878733660e3adb2944076f88ec
Muskan Shroti <shrotimuskan@gmail.com>
2025-12-19 18:26:17 +0530

    Add countTotalReportsForUser method
    Added a method to count total reports for a user.

--- ProjectHON/src/main/java/com/example/ProjectHON/Chat_Report/ChatReportRepository.java
+++ ProjectHON/src/main/java/com/example/ProjectHON/Chat_Report/ChatReportRepository.java
@@ -1,7 +1,18 @@
 package com.example.ProjectHON.Chat_Report;
 
+import com.example.ProjectHON.Whisper_masterpackage.WhisperMaster;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import software.amazon.awssdk.services.rekognition.endpoints.internal.Value;
 
 public interface ChatReportRepository extends JpaRepository<ChatReport , Integer> {
+    @Query("SELECT COUNT(c) FROM ChatReport c WHERE c.owner.userId = :ownerId")
+    int countTotalReportsForUser(@Param("ownerId") Long ownerId);
+
+
+//    @Query("SELECT c.reason FROM ChatReport c WHERE c.whisper.whisperId = :whisperId")
+//    String findReasonByWhisperId(@Param("whisperId") Long whisperId);
+
+
 }
