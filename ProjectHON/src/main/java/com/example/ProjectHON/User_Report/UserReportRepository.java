e061ecb777a0108d45524c56c9f7fb8cf57f8965
Muskan Shroti <shrotimuskan@gmail.com>
2025-12-19 18:30:32 +0530

    Enhance UserReportRepository with new queries

--- ProjectHON/src/main/java/com/example/ProjectHON/User_Report/UserReportRepository.java
+++ ProjectHON/src/main/java/com/example/ProjectHON/User_Report/UserReportRepository.java
@@ -1,6 +1,25 @@
 package com.example.ProjectHON.User_Report;
 
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
-public interface UserReportRepository extends JpaRepository<UserReport , Integer> {
+import java.util.List;
+
+public interface UserReportRepository extends JpaRepository<UserReport , Long> {
+
+    @Query("select count(c) from UserReport  c where c.ownerUser.userId =:ownerId")
+    int countTotalReportsForUser(@Param("ownerId") Long ownerId);
+
+    @Modifying
+    @Transactional
+    @Query("delete from UserReport d where (d.ownerUser.userId = :ownerId and d.reporterUser.userId =:reporterId)")
+    void deleteFromUserReport(@Param("ownerId") Long ownerId , @Param("reporterId") Long reporterId);
+
+    @Query("SELECT c FROM UserReport c WHERE c.reporterUser.userId = :reporterId")
+    List<UserReport> findAllByReporterId(@Param("reporterId") Long reporterId);
+
 }
+
