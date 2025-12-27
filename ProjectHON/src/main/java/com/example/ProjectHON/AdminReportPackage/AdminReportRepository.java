c3376417554c7518a338409fbafdc4b4ede09df2
Muskan Shroti <shrotimuskan@gmail.com>
2025-12-27 23:37:06 +0530

    Update AdminReportRepository.java

--- ProjectHON/src/main/java/com/example/ProjectHON/AdminReportPackage/AdminReportRepository.java
+++ ProjectHON/src/main/java/com/example/ProjectHON/AdminReportPackage/AdminReportRepository.java
@@ -1,8 +1,17 @@
 package com.example.ProjectHON.AdminReportPackage;
 
+import com.example.ProjectHON.User_Report.UserReport;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface AdminReportRepository extends JpaRepository<AdminReportEntity , Integer> {
 
+//    Optional<AdminReportEntity> findByUserReport(UserReport userReport);
+
+    @Query("SELECT a FROM AdminReportEntity a WHERE a.userReport.id = :reportId")
+    Optional<AdminReportEntity> findByUserReportId(@Param("reportId") Long reportId);
 
 }
