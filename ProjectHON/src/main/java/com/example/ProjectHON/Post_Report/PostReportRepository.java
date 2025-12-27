c56300192b5ea86c312a40b86c9709bd6c8c8e69
Muskan Shroti <shrotimuskan@gmail.com>
2025-12-27 23:38:31 +0530

    Enhance PostReportRepository with new query methods
    Added custom query methods for retrieving and managing PostReport entities.

--- ProjectHON/src/main/java/com/example/ProjectHON/Post_Report/PostReportRepository.java
+++ ProjectHON/src/main/java/com/example/ProjectHON/Post_Report/PostReportRepository.java
@@ -1,8 +1,33 @@
 package com.example.ProjectHON.Post_Report;
 
+import com.example.ProjectHON.Post_masterpackage.PostMaster;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface PostReportRepository extends JpaRepository<PostReport , Integer> {
     boolean existsByReporterUserIdAndPostreport_PostId(Long reporterId, Long postId);
+//    List<PostMaster>findBy
+
+
+    @Query("SELECT pr.postreport FROM PostReport pr WHERE pr.reporter.userId = :userId")
+    List<PostMaster> findPostMasterByUserId(@Param("userId") Long userId);
+
+    void deleteByPostreport(PostMaster post);
+    long countByPostreport(PostMaster post);
+
+
+    List<PostReport> findAllByReporter_UserId(Long userId);
+
+    long countByReporter_UserIdAndStatus(
+            Long userId,
+            PostReport.ReportStatus status
+    );
+
+
+
+
 
 }
